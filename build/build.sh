462b383225f367cd184641b9e9036769ce5017ba
Elena Huma <85832851+IoanaElenaH@users.noreply.github.com>
2024-03-17 22:42:19 +0200

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$0")/.."
 
 # Build the application
 export GO111MODULE=auto
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o bin/commenter-linux-amd64 .
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o bin/commenter-linux-amd64 ./cmd/commenter
